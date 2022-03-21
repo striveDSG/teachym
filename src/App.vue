cf949e5788b22362402bc107a505013f3530ee30
striveDSG <1509440167@qq.com>
2022-03-21 23:51:30 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/" tag="a">Home</router-link> |
+      <router-link :to="rout" replace>About</router-link>
+      <button @click="handlerClick">about</button>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rout: "/about",
+    };
+  },
+  methods: {
+    handlerClick() {
+      // this.$router.push("/about");
+      let id = 2,
+        name = "yt";
+      this.$router.push({
+        path: "/about",
+        query: {
+          id,
+          name,
+        },
+      });
+    },
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    meta: { title: "Home" },
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Home.vue"),
+  },
+  {
+    path: "/about",
+    name: "About",
+    meta: { title: "about" },
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "*",
+    redirect: "/",
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  window.document.title = to.meta.title;
+  next();
+});
+router.afterEach(() => {
+  NProgress.done();
+});
+export default router;
